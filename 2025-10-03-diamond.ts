ff14d9f9e675fdadb62468194b0fb72366c924d3
Michelle <miiitiiigg@gmail.com>
2025-10-06 14:46:05 +0800

    refactor: diamond

--- 2025-10-03-diamond.ts
+++ 2025-10-03-diamond.ts
@@ -23,35 +23,35 @@
 //   output3 += "\n";
 // }
 
-function diamond(numOfRows3: number){
-    let middle: number = (numOfRows3 + 1) / 2;
-    let output: string = "\n";
+function diamond(numOfRows3: number) {
+  let middle: number = (numOfRows3 + 1) / 2;
+  let output: string = "\n";
 
-    // 檢驗參數是否為奇數
-    if(numOfRows3 % 2 === 0){
-     alert("Input number must be odd.");
-     return;
-    }
+  // 檢驗參數是否為奇數
+  if (numOfRows3 % 2 === 0) {
+    alert("Input number must be odd.");
+    return;
+  }
 
-    for(let row: number = 1; row <= numOfRows3; row++){
-       if(row <= middle){
-        for(let i: number = (numOfRows3 - 1) / 2; i >= row; i--){
-            output += " ";
-        }
-        
-        for(let j: number = 1; j <= row; j++){
-            if(j === 1) output += "*";
-            else output += "**";
-        }
+  for (let row: number = 1; row <= numOfRows3; row++) {
+    if (row <= middle) {
+      for (let i: number = (numOfRows3 - 1) / 2; i >= row; i--) {
+        output += " ";
+      }
+
+      for (let j: number = 1; j <= row; j++) {
+        if (j === 1) output += "*";
+        else output += "**";
+      }
     } else {
-        for(let i: number = 1; i <= row - middle; i++){
-            output += " ";
-        }
-        
-        for(let j: number = numOfRows3; j >= row; j--){
-            if(j === numOfRows3) output += "*";
-            else output += "**";
-        }
+      for (let i: number = 1; i <= row - middle; i++) {
+        output += " ";
+      }
+
+      for (let j: number = numOfRows3; j >= row; j--) {
+        if (j === numOfRows3) output += "*";
+        else output += "**";
+      }
     }
     output += "\n";
   }
@@ -61,42 +61,42 @@ console.log(diamond(5));
 
 // 2. while
 function diamond2(numOfRows4: number) {
-    let middle: number = (numOfRows4 + 1) / 2;
-    let row: number = 1;
-    let output: string = "\n";
+  let middle: number = (numOfRows4 + 1) / 2;
+  let row: number = 1;
+  let output: string = "\n";
 
-   // 檢驗參數是否為奇數
-    if(numOfRows4 % 2 === 0){
-      alert("Input number must be odd.");
-      return;
-    }
+  // 檢驗參數是否為奇數
+  if (numOfRows4 % 2 === 0) {
+    alert("Input number must be odd.");
+    return;
+  }
 
-  while(row <= numOfRows4){
-    if(row <= middle){
-        let i: number = (numOfRows4 - 1) / 2;
-        let j: number = 1;
-        while(i >= row){
-            output += " ";
-            i--;
-        }
-        while(j <= row){
-            if(j === 1) output += "*";
-            else output += "**";
-            j++;
-        }
+  while (row <= numOfRows4) {
+    if (row <= middle) {
+      let i: number = (numOfRows4 - 1) / 2;
+      let j: number = 1;
+      while (i >= row) {
+        output += " ";
+        i--;
+      }
+      while (j <= row) {
+        if (j === 1) output += "*";
+        else output += "**";
+        j++;
+      }
     } else {
-        let i: number = 1;
-        let j: number = numOfRows4;
-        while(i <= row - middle){
-            output += " ";
-            i++
-        }
+      let i: number = 1;
+      let j: number = numOfRows4;
+      while (i <= row - middle) {
+        output += " ";
+        i++;
+      }
 
-        while(j >= row){
-            if(j === numOfRows4) output += "*";
-            else output += "**";
-            j--;
-        }
+      while (j >= row) {
+        if (j === numOfRows4) output += "*";
+        else output += "**";
+        j--;
+      }
     }
     output += "\n";
     row++;
@@ -107,4 +107,79 @@ function diamond2(numOfRows4: number) {
 console.log(diamond2(5));
 
 // 問題：
-// 迴圈印星號：只有第一次印1個，之後每次迴圈都印2個，主體邏輯要怎麼寫？
+// 迴圈印星號：只有第一次印1個，之後每次迴圈都印2個，主體邏輯要怎麼寫？ => 改良版
+// 3. for改良版
+function diamond3(numOfRows: number) {
+  let output: string = "\n";
+  let middle: number = Math.ceil(numOfRows / 2);
+  let row: number = 1;
+  let spaces: number, stars: number;
+
+  if (numOfRows % 2 === 0) {
+    alert("Input number must be odd.");
+    return;
+  }
+
+  for (row = 1; row <= numOfRows; row++) {
+    if (row <= middle) {
+      spaces = middle - row;
+      stars = 2 * row - 1;
+    } else {
+      spaces = row - middle;
+      stars = 2 * (numOfRows - row) + 1;
+    }
+
+    for (let s = 1; s <= spaces; s++) {
+      output += " ";
+    }
+
+    for (let r = 1; r <= stars; r++) {
+      output += "*";
+    }
+    output += "\n";
+  }
+  return output;
+}
+
+console.log(diamond3(13));
+
+// 4. while改良版
+function diamond4(numOfRows: number) {
+  let output: string = "\n";
+  let middle: number = Math.ceil(numOfRows / 2);
+  let row: number = 1;
+
+  if (numOfRows % 2 === 0) {
+    alert("Input number must be odd.");
+    return;
+  }
+
+  while (row <= numOfRows) {
+    let spaces: number, stars: number;
+    let s: number = 1;
+    let r: number = 1;
+
+    if (row <= middle) {
+      spaces = middle - row;
+      stars = 2 * row - 1;
+    } else {
+      spaces = row - middle;
+      stars = 2 * (numOfRows - row) + 1;
+    }
+
+    while (s <= spaces) {
+      output += " ";
+      s++;
+    }
+
+    while (r <= stars) {
+      output += "*";
+      r++;
+    }
+    output += "\n";
+    row++;
+  }
+  return output;
+}
+
+console.log(diamond4(9));

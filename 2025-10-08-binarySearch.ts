4aaa8fe39db88c1269f494ddd8af9067053097fb
Michelle <miiitiiigg@gmail.com>
2025-10-09 06:29:49 +0800

    binary search: add for loop + modify middleIndex

--- 2025-10-08-binarySearch.ts
+++ 2025-10-08-binarySearch.ts
@@ -2,27 +2,32 @@
 //  4.1 Assume `a` is sorted ascendingly.
 
 function binarySearch(a: Array<number>, target: number): boolean {
-    let endIndex = a.length;
-    let startIndex = 0;
-    let middleIndex = Math.floor(endIndex - startIndex / 2);
+  let startIndex = 0;
+  let endIndex = a.length - 1;
+  let middleIndex = Math.floor((endIndex - startIndex) / 2);
+  console.log("startIndex:", startIndex);
+  console.log("endIndex:", endIndex);
+  console.log("middleIndex:", middleIndex);
 
-    if(target === a[middleIndex]) {
-        return true;
-        console.log(startIndex, endIndex, middleIndex);
-    } else if(target < a[middleIndex]) {
-        endIndex = middleIndex;
-        middleIndex = Math.floor(middleIndex /= 2);
-        console.log(startIndex, endIndex, middleIndex);
-    } else if(target > a[middleIndex]) {
-        startIndex = middleIndex;
-        middleIndex = Math.floor(middleIndex *= 2);
-        console.log(startIndex, endIndex, middleIndex);
+  for (let i = 0; i < a.length; i += middleIndex) {
+    if (target === a[middleIndex]) {
+      console.log("C1:", startIndex, endIndex, middleIndex);
+      return true;
+    } else if (target < a[middleIndex]) {
+      endIndex = middleIndex;
+      middleIndex = Math.floor((endIndex - startIndex) / 2);
+      console.log("C2:", startIndex, endIndex, middleIndex);
+    } else if (target > a[middleIndex]) {
+      startIndex = middleIndex;
+      middleIndex = Math.floor(middleIndex + (endIndex - startIndex) / 2);
+      console.log("C3:", startIndex, endIndex, middleIndex);
     }
-
-    return false;
+    console.log("i:", i);
+  }
+  return false;
 }
 
-console.log(binarySearch([-5, 2, 3, 4, 10], 5)); // false
-// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 7)); // true
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9)); // true (middleIndex: 4 => 6 => 8)
+// console.log(binarySearch([-5, 2, 3, 4, 10], 5)); // false
 // console.log(binarySearch([1], 1)); // true
 // console.log(binarySearch([23, 98, 105], 0)); // false

f925ed52dc6553f82f55b83444ec1bd3c093d655
Michelle <miiitiiigg@gmail.com>
2025-10-20 06:35:10 +0800

    findMaxIndex change from sort to find max number

--- 2025-10-17-findMaxIndex.ts
+++ 2025-10-17-findMaxIndex.ts
@@ -1,25 +1,33 @@
 import { copyArray } from "./2025-10-16-getMedian.ts";
 
-function findMaxIndex(a: Array<number>): Array<number> | null {
-    // 檢查空陣列
-    if (a.length === 0) {
-        return null;
-    }
+function findMaxIndex(arr: Array<number>): Array<number> | null {
+  // 檢查空陣列
+  if (arr.length === 0) {
+    return null;
+  }
 
-    const arr2: Array<number> = copyArray(a);
-    let output: Array<number> = [];
+  const arr2: Array<number> = copyArray(arr);
+  //   console.log("arr2:", arr2);
+  const output: Array<number> = [];
 
-    for (let i: number = 0, j: number = 0; i < a.length; i++) {
-        if (arr2[i] > arr2[i + 1] || arr2[i] === arr2[i + 1]) {
-            output[j] = i;
-            arr2[i + 1] = arr2[i];
-            j++;
-        }
+  for (let i: number = 0; i < arr2.length - 1; i++) {
+    // 將最大值移到陣列末端
+    if (arr2[i] > arr[i + 1]) {
+      [arr2[i], arr2[i + 1]] = [arr2[i + 1], arr2[i]];
     }
-    return output;
+  }
+
+  for (let i: number = 0, j: number = 0; i < arr.length; i++) {
+    // 將最大值推到結果陣列
+    if (arr[i] === arr2[arr2.length - 1]) {
+      output[j] = i;
+      j++;
+    }
+  }
+  return output;
 }
 
-console.log(findMaxIndex([1, 2, 6, 4, 6])); // [2, 4] (6)
-// console.log(findMaxIndex([14, 3])); // [0] (14)
-// console.log(findMaxIndex([8, 8, 8, 0, 3, -2])); // [0, 1, 2] (8)
-// console.log(findMaxIndex([-2, -2, -10])); // [0, 1] (-2)
+console.log(findMaxIndex([1, 2, 6, 4, 8, 6, 8])); // [4, 6] (8)
+console.log(findMaxIndex([14, 3])); // [0] (14)
+console.log(findMaxIndex([8, 8, 8, 0, 3, -2])); // [0, 1, 2] (8)
+console.log(findMaxIndex([-2, -2, -10])); // [0, 1] (-2)
